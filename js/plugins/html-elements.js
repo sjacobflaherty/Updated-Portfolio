0806a07d39e4ca69b8bf97c308e2ce45fd3dadc0
Jacob Flaherty <98563031+sjacobflaherty@users.noreply.github.com>
2023-09-07 23:03:56 -0700

    updates

--- js/plugins/html-elements.js
+++ js/plugins/html-elements.js
@@ -1,11 +1,11 @@
 $(function () {
-  $('passNavbar').load('/components/navbar.html');
-  $('passDadJokes').load('/components/dadjokes.html');
-  $('passFooter').load('/components/footer.html');
+  $('passNavbar').load('components/navbar.html');
+  $('passDadJokes').load('components/dadjokes.html');
+  $('passFooter').load('components/footer.html');
 });
 
-$(function () {
-  $('passProjectNavbar').load('../components/navbar.html');
-  $('passProjectDadJokes').load('../components/dadjokes.html');
-  $('passProjectFooter').load('../components/footer.html');
-});
+// $(function () {
+//   $('passProjectNavbar').load('../components/navbar.html');
+//   $('passProjectDadJokes').load('../components/dadjokes.html');
+//   $('passProjectFooter').load('../components/footer.html');
+// });
