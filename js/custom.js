ad760182754fa543ad5197600cdb51ae56c18624
Jacob Flaherty <98563031+sjacobflaherty@users.noreply.github.com>
2023-09-13 21:40:14 -0700

    updates

--- js/custom.js
+++ js/custom.js
@@ -855,7 +855,7 @@ $(function () {
         responsive: {
           0: {
             margin: 10,
-            items: 2,
+            items: 3,
           },
           600: {
             items: 3,
